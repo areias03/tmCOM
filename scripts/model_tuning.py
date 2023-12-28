754dbc212832f97deb58a140545ae426f786868c
areias03 <alexandreareias1718@gmail.com>
2023-12-28 21:12:47 +0000

    Added plots

--- scripts/model_tuning.py
+++ scripts/model_tuning.py
@@ -1,6 +1,6 @@
 from typing import List
 import seaborn as sns
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 import pandas as pd
 import numpy as np
@@ -8,7 +8,7 @@ from cobra.io import read_sbml_model
 from cobra.io.sbml import Model
 from mewpy.simulation import Environment, Simulator, get_simulator
 
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ProcessPoolExecutor
 
 
 ec_bt = read_sbml_model('../models/ec/ec_Bacteroides_thetaiotaomicron_VPI_5482.xml')
@@ -35,15 +35,18 @@ def prot_pool_analysis(model: Model):
 
     model_id = str(model.id.strip('M_'))
     sim: Simulator = get_simulator(model)
+    print('Started simulator')
     env = Environment.complete(sim, max_uptake=1000.0, inplace=False)
     sim.set_environmental_conditions(env)
     for i in range(len(l_va)):
         cons = constraint_list[i]
         solution = sim.simulate(constraints=cons)
         sol_list.append(solution.objective_value)
+    print('Finished simulations')
     data = {'Protein pool exchange': ind_list, model_id: sol_list}
     df = pd.DataFrame(data)
     df = df.set_index('Protein pool exchange')
+    print(f'Finished iteration for {model_id}')
     return df
 
 
@@ -51,13 +54,13 @@ if __name__ == "__main__":
 
     merged: List = []
 
-    with ThreadPoolExecutor() as p:
+    with ProcessPoolExecutor() as p:
         solution_list = []
         for result in p.map(prot_pool_analysis, m_list):
             merged.append(result)
         df = pd.concat(merged, axis=1)
-        df.to_csv('../data/model_tuning/prot_pool_all.csv')
+        df.to_csv('../data/model_tuning/prot_pool_all2.csv')
         print(df)
         df_norm = (df - df.mean()) / df.std()
         sns.heatmap(df_norm)
-        plt.savefig('../data/model_tuning/prot_pool_all.png')
+        plt.savefig('../data/model_tuning/prot_pool_all_hm.png')
--- scripts/prot_pool.py
+++ scripts/prot_pool.py
@@ -3,17 +3,19 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+import matplotlib.gridspec
 
 path = '../data/model_tuning/'
 
 files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+files = sorted(files)
 ind_list = []
 merged = []
 
 l_va = np.linspace(0.1, 1.0, num=10)
 
 for i in range(len(l_va)):
-        ind_list.append(f'0 - {round(l_va[i], 1)}')
+        ind_list.append(f'{round(l_va[i], 1)}')
         
 ind = {'Protein pool exchange': ind_list}
 
@@ -27,23 +29,62 @@ for p in files:
     r = pd.read_csv(p)
     r = r.drop(r.columns[0],axis=1)
     merged.append(r)
+    
 df = pd.concat(merged, axis=1)
 df.to_csv('../data/model_tuning/results/prot_pool_all.csv')
 df = df.set_index('Protein pool exchange')
+df = df.loc[:,~df.columns.duplicated()]
 print(df)
-#df = df.drop(df.columns[0],axis=1)
-#df_norm = df.apply(lambda x: (x-x.mean())/x.std(), axis = 1)
-#print(df_norm)
-sns.heatmap(df)
-plt.savefig('../data/model_tuning/results/prot_pool_all.png', bbox_inches='tight')
-    
 '''
-for result in p.map(prot_pool_analysis, m_list):
-            merged.append(result)
-        df = pd.concat(merged, axis=1)
-        df.to_csv('../data/model_tuning/prot_pool_all.csv')
-        print(df)
-        df_norm = (df - df.mean()) / df.std()
-        sns.heatmap(df_norm)
-        plt.savefig('../data/model_tuning/prot_pool_all.png')
+
+# Generate clustermap
+
+x_labels = ['Bt VPI-5482','Bu ATCC-8492','Ec ED1a','Fn ATCC-25586','Ri L1-82','Sp ATCC-15912','Ss DSM-20560']
+g = sns.clustermap(df, row_cluster = False, vmin = 0, xticklabels = x_labels)
+g.tick_params(axis='both', which='both', length=0)
+g.ax_heatmap.set_ylabel(g.ax_heatmap.get_ylabel(), labelpad = 20)
+plt.setp(g.ax_heatmap.get_yticklabels(), rotation=0)  # For y axis
+plt.setp(g.ax_heatmap.get_xticklabels(), rotation=90) # For x axis
+plt.savefig('../data/images/prot_pool_all.png', bbox_inches='tight')
+'''
+
+df_inv = df.T
+print(df_inv)
+
+# Generate violin plot
+v = sns.violinplot(data=df_inv, inner="point", palette="Blues")
+v.tick_params(axis='both', which='both', length=0)
+v.set_ylabel("Growth", labelpad = 15)
+v.set_xlabel(v.get_xlabel(), labelpad = 15)
+plt.setp(v.get_yticklabels(), rotation=0)  # For y axis
+plt.setp(v.get_xticklabels(), rotation=90) # For x axis
+plt.savefig('../data/images/prot_pool_violin.png', bbox_inches='tight')
+
+'''
+df_inv = df.T
+print(df_inv)
+
+x_labels = ['Bt VPI-5482','Bu ATCC-8492','Ec ED1a','Fn ATCC-25586','Ri L1-82','Sp ATCC-15912','Ss DSM-20560']
+
+#First create the clustermap figure
+g = sns.clustermap(df, row_cluster = False, vmin = 0, xticklabels = x_labels, figsize=(15,10))
+g.tick_params(axis='both', which='both', length=0)
+g.ax_heatmap.set_ylabel(g.ax_heatmap.get_ylabel(), labelpad = 20)
+plt.setp(g.ax_heatmap.get_yticklabels(), rotation=0)  # For y axis
+plt.setp(g.ax_heatmap.get_xticklabels(), rotation=90) # For x axis
+# set the gridspec to only cover half of the figure
+g.gs.update(left=0.05, right=0.45)
+
+#create new gridspec for the right part
+gs2 = matplotlib.gridspec.GridSpec(1,1, left=0.6)
+# create axes within this new gridspec
+ax2 = g.fig.add_subplot(gs2[0])
+# plot boxplot in the new axes
+v = sns.violinplot(data=df_inv, inner="point", ax = ax2)
+v.tick_params(axis='both', which='both', length=0)
+v.set_ylabel("Growth", labelpad = 15)
+v.set_xlabel(v.get_xlabel(), labelpad = 15)
+plt.setp(v.get_yticklabels(), rotation=0)  # For y axis
+plt.setp(v.get_xticklabels(), rotation=90) # For x axis
+plt.savefig('../data/images/prot_pool.png', bbox_inches='tight')
 '''
